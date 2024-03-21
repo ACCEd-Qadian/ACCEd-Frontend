08a8d7b1b2c8eb37128038d41432614e78cf95e4
ACCEd-Qadian <163812472+ACCEd-Qadian@users.noreply.github.com>
2024-03-21 11:23:12 +0530

    Update Login.jsx

--- src/Components/Pages/Login/Login.jsx
+++ src/Components/Pages/Login/Login.jsx
@@ -2,103 +2,100 @@ import React, { useEffect, useState } from "react";
 import { Container, Form, Button } from "react-bootstrap";
 import "./Login.css";
 import { Link } from "react-router-dom";
-import axios from "axios"
-import Alert from 'react-bootstrap/Alert';
-import  { Redirect } from 'react-router-dom'
-
+import axios from "axios";
+import Alert from "react-bootstrap/Alert";
 
 const Login = (props) => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [alert, setAlert] = useState({
-    message:"",
-    variant:"",
-    value:false
+    message: "",
+    variant: "",
+    value: false
   });
 
   const [loggedinuser, setLoggedinuser] = useState();
-
-function Redirecttologedin() {
-     window.location.href = "https://acced.netlify.app";
-}
-
-function RedirecttoAdminpanel() {
-       window.location.href = "https://acced.netlify.app";
-}
-
-  const handleLogin = async(e) => {
-    e.preventDefault();
-    console.log(email, password);
-    const newemail = email.toLowerCase()
-    try {
-      const response = await axios.post("https://stormy-singlet-crow.cyclic.app/login", {newemail, password})
-      localStorage.setItem('user', JSON.stringify(response.data));
-      setLoggedinuser(response.data)
-      console.log(response)
-      setAlert({
-          message: "Loged in",
-          variant:"success",
-          value:true
-        })
-        if (response.data.isAdmin) {
-          RedirecttoAdminpanel()
-        }else{
-          Redirecttologedin()
-        }
-  } catch (error) {
-      console.log(error)
-      setAlert({
-          message: error.response.data,
-          variant:"danger",
-          value:true
-        })
+  // console.log(window.location.origin)
+  function Redirecttologedin() {
+    window.location.href = "https://acced.netlify.app";
   }
-  };
 
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await axios.post("https://acced.cyclic.app/login", {
+        email,
+        password
+      });
+      localStorage.setItem("user", JSON.stringify(response.data));
+      setLoggedinuser(response.data);
+      console.log(response);
+      setAlert({
+        message: "Loged in",
+        variant: "success",
+        value: true
+      });
+        Redirecttologedin();
+    } catch (error) {
+      console.log(error);
+      setAlert({
+        message: error.response.data.message,
+        variant: "danger",
+        value: true
+      });
+    }
+  };
 
   return (
     <div className="login-box">
-    <Container className="LoginContainer">
-    {alert.value && 
-    <Alert variant={alert.variant} onClose={()=>{setAlert({value:false})}} dismissible>
-      <Alert.Heading>{alert.message}</Alert.Heading>
-    </Alert>
-    }
-      <h2>Login</h2>
-      <Form onSubmit={handleLogin} className="">
-        <Form.Group controlId="formBasicemail">
-          <Form.Label>Email Address</Form.Label>
-          <Form.Control
-            type="email"
-            placeholder="Enter Email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            required={true}
-            autoComplete="off"
-          />
-        </Form.Group>
-        <Form.Group controlId="password">
-          <Form.Label>Password</Form.Label>
-          <Form.Control
-            type="password"
-            placeholder="Enter Password"
-            value={password}
-            onChange={(e) => {
-              setPassword(e.target.value);
+      <Container className="LoginContainer">
+        {alert.value && (
+          <Alert
+            variant={alert.variant}
+            onClose={() => {
+              setAlert({ value: false });
             }}
-            required={true}
-          />
-        </Form.Group>
-        <Button type="submit" className="btn btn-primary" variant="Primary">
-          Login
-        </Button>
-        <div>
+            dismissible
+          >
+            <Alert.Heading>{alert.message}</Alert.Heading>
+          </Alert>
+        )}
+        <h2>Login</h2>
+        <Form onSubmit={handleLogin} className="">
+          <Form.Group controlId="formBasicemail">
+            <Form.Label>Email Address</Form.Label>
+            <Form.Control
+              type="email"
+              placeholder="Enter Email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              required={true}
+              autoComplete="off"
+            />
+          </Form.Group>
+          <Form.Group controlId="password">
+            <Form.Label>Password</Form.Label>
+            <Form.Control
+              type="password"
+              placeholder="Enter Password"
+              value={password}
+              onChange={(e) => {
+                setPassword(e.target.value);
+              }}
+              required={true}
+            />
+          </Form.Group>
+          <Button type="submit" className="btn btn-primary" variant="Primary">
+            Login
+          </Button>
           <div>
-            New customer? <Link to={"/Register"}>Create your account</Link>
+            <div>
+              New customer? <Link to={"/Register"}>Create your account</Link>
+            </div>
           </div>
-        </div>
-      </Form>
-    </Container>
+        </Form>
+      </Container>
     </div>
   );
 };
